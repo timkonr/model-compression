d7350e8991c5d65b5b608ccdeae9de6677f07c92
timkonr <tim.konrad@gmx.at>
2025-01-20 14:07:42 +0100

    add debug info

--- src/inference.py
+++ src/inference.py
@@ -1,4 +1,4 @@
-from torch import qint8
+from torch import qint8, tensor
 from torch.nn import Linear
 from torch.quantization import quantize_dynamic
 from conette import CoNeTTEConfig, CoNeTTEModel
@@ -11,6 +11,15 @@ quantized_model = quantize_dynamic(
 )
 quantized_model.to("cpu")
 path = "data/CLOTHO_v2.1/clotho_audio_files/evaluation/"
-outputs = quantized_model([path + "105bpm.wav", path + "2013622thunder.wav"], sr=[44100, 44100], task="clotho")
+
+audio = [path + "105bpm.wav", path + "2013622thunder.wav"]
+sr = [44100, 44100]
+
+print(f"Model is on device: {next(quantized_model.parameters()).device}")
+tensor = tensor([1.0])
+print(f"Default tensor device: {tensor.device}")
+
+
+outputs = quantized_model(audio, sr=sr, task="clotho")
 
 print(outputs)
