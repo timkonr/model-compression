1e0a97980fe4a92bff56ba1695da2c3f74f6cf4f
timkonr <tim.konrad@gmx.at>
2025-01-21 11:38:36 +0100

    download dataset

--- src/prepare.py
+++ src/prepare.py
@@ -1,10 +1,16 @@
 from conette import CoNeTTEConfig, CoNeTTEModel
+from aac_datasets import Clotho
+import os
 
 def main():
+    # Download model
     config = CoNeTTEConfig.from_pretrained("Labbeti/conette")
     model = CoNeTTEModel.from_pretrained("Labbeti/conette", config=config)
-
     model.save_pretrained("./model/")
+    
+    # Download dataset
+    os.makedirs("data", exist_ok=True)
+    clotho_ev_ds = Clotho("data", subset="eval", download=True)
 
 if __name__ == "__main__":
     main()
