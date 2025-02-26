3bb2ff8f02ed568a92745fbac148bead6f7d27a1
timkonr <tim.konrad@gmx.at>
2025-02-26 17:55:50 +0700

    delete all corrupted files

--- src/prepare.py
+++ src/prepare.py
@@ -1,6 +1,7 @@
 from conette import CoNeTTEConfig, CoNeTTEModel
-from aac_datasets import Clotho
+from aac_datasets import Clotho, AudioCaps
 import os
+import torchaudio
 
 
 def main():
@@ -12,6 +13,21 @@ def main():
     # Download dataset
     os.makedirs("data", exist_ok=True)
     Clotho("data", subset="eval", download=True)
+    AudioCaps("data", subset="val", download=True, verify_files=True)
+    remove_corrupted_files()
+
+
+def remove_corrupted_files():
+    for filename in os.listdir("./data/AUDIOCAPS/audio_32000Hz/val"):
+        if filename.endswith(".flac"):
+            file_path = os.path.join("./data/AUDIOCAPS/audio_32000Hz/val", filename)
+            try:
+                # Attempt to load the audio file
+                torchaudio.load(file_path)
+            except Exception as e:
+                # If an error occurs, delete the corrupted file
+                print(f"Deleting corrupted file: {file_path}, Error: {e}")
+                os.remove(file_path)
 
 
 if __name__ == "__main__":
